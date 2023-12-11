d0fc9e658c136de858661cf835f3cdd93f795da1
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-12-11 19:35:10 +0530

    cards

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
-    <div className='w-48 m-2 h-auto hover:border-[1px] p-1  border-solid border-zinc-500'>
-      <img className='p-1' src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="bowl" />
-      <h3>Meghna Food</h3>
-      <h4>Biryani,North India,Asian</h4>
-      <h5>4.4 stars</h5>
-      <h5>38 minutes</h5>
+    <div className='w-48 m-2 h-auto p-2 transition-all hover:scale-105 hover:shadow-lg'>
+      <img className='p-1 ' src={props.url} alt="bowl" />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h5>{props.rating} stars</h5>
+      <h5>{props.time} minutes</h5>
     </div>
   )
 }
@@ -22,8 +22,14 @@ export default function Body() {
             </div>
         </div>
         <div className='flex flex-wrap gap-2'>
-          < RestaurantCard />
-          < RestaurantCard />
+          < RestaurantCard 
+          url="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          resName="Meghna Food" cuisine="Biryani,North India,Asian" rating="4.4" time="38"
+          />
+          < RestaurantCard 
+          url="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          resName="Delite Bowl" cuisine="Egg Curry,North India,Asian" rating="4.6" time="18"
+          />
         </div>
         
 
