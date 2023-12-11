9f9ac53254b2693e4a56948cdb7f5a65cf0dc64a
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-12-12 01:27:52 +0530

    📝 readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # namste-food
+
  Namaste Food using react
+
+## Header
+  
+    logo
+    Nav Items
+
+## Body
+  
+    search
+    restaurant list
+
+    restaurant cards
+      Img
+      Name,Star Rating,cuisine,price,delivery time
+
+## footers
+  
+    copyright
+    address
+    contact
--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,6 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Body from './components/Body.jsx';
 
-/*
-Header
-  logo
-  Nav Items
-Body
-  search
-  restaurant list
-    restaurant cards
-      Img
-      Name,Star Rating,cuisine,price,delivery time
-footers
-  copyright
-  address
-  contact
-*/
 
 export default function App() {
   return (
