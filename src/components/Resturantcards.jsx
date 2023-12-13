4c657516409b7f5b0f53956d0718ac6393389cb4
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-12-13 23:38:34 +0530

    🚑

---
+++ src/components/Resturantcards.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const RestaurantCard = (props) => {
+
+    const{
+      name,
+      cuisine,
+      rating,
+      time,
+      image
+    } = {...props}
+    
+    return (
+      <div className='w-48 m-2 h-auto p-2 border-solid border-2 cursor-pointer border-zinc-500  transition-all hover:scale-105 hover:shadow-lg'>
+        <img className='p-1 ' src={image} alt="bowl" />
+        <h3>{name}</h3>
+        <h4>{cuisine}</h4>
+        <h5>{rating} stars</h5>
+        <h5>{time} minutes</h5>
+      </div>
+    )
+  }
+
+export default RestaurantCard;
