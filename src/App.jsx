be3390eb9ff0d026001cf314724761416268a9d8
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-12-28 21:19:12 +0530

    ➕ Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Body from './components/Body.jsx';
-
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
 
 export default function App() {
   return (
-    <div className=''>
-      < Header />
-      < Body />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
