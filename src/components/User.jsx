5189540f80222873e160ba7052bc9a6183ec944c
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2024-01-10 23:40:16 +0530

    🍱 Add count state to User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,12 @@
+import React, { useState } from 'react';
+
+
 const user = ({name}) => {
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="p-2 m-2 border">
+      <h1>Count = {count}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: India</h3>
       <h4>Conatct: sakshamtyagi2008@gmail.com</h4>
